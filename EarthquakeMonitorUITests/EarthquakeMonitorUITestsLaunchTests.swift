e76e003c086a0ce1dae6af184e2783df79c44999
victor <victorkibichy>
2024-06-12 12:00:31 +0300

    Initial Commit

---
+++ EarthquakeMonitorUITests/EarthquakeMonitorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EarthquakeMonitorUITestsLaunchTests.swift
+//  EarthquakeMonitorUITests
+//
+//  Created by  Bouncy Baby on 6/12/24.
+//
+
+import XCTest
+
+final class EarthquakeMonitorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
