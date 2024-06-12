dcb9a98cccdd6cd826d05e03f9ba299ccb0a6e7b
victor <victorkibichy>
2024-06-12 12:37:00 +0300

    Creation of the EarthquakeVC

---
+++ EarthquakeMonitor/ViewControllers/EarthquakeViewController.swift
@@ -0,0 +1,63 @@
+//
+//  EarthquakeViewController.swift
+//  EarthquakeMonitor
+//
+//  Created by  Bouncy Baby on 6/12/24.
+//
+
+import Foundation
+import UIKit
+import Combine
+
+class EarthquakeViewController: UIViewController {
+    private let tableView = UITableView()
+    private let viewModel = EarthquakeViewModel()
+    private var cancellables: Set<AnyCancellable> = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        bindViewModel()
+        viewModel.fetchEarthquakes()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .white
+        title = "Earthquakes"
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    private func bindViewModel() {
+        viewModel.$earthquakes
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                self?.tableView.reloadData()
+            }
+            .store(in: &cancellables)
+    }
+}
+
+extension EarthquakeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.earthquakes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let earthquake = viewModel.earthquakes[indexPath.row]
+        cell.textLabel?.text = "\(earthquake.magnitude) - \(earthquake.place)"
+        return cell
+    }
+}
---
+++ EarthquakeMonitor/ViewModels/EarthquakeViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  EarthquakeViewModel.swift
+//  EarthquakeMonitor
+//
+//  Created by  Bouncy Baby on 6/12/24.
+//
+
+
+import Foundation
+import Combine
+
+class EarthquakeViewModel {
+    @Published var earthquakes: [Earthquake] = []
+    private var cancellables: Set<AnyCancellable> = []
+    
+    func fetchEarthquakes() {
+        let urlString = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2022-01-01&endtime=2022-01-02"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map { $0.data }
+            .decode(type: EarthquakeResponse.self, decoder: JSONDecoder())
+            .map { response in
+                response.features.map { feature in
+                    Earthquake(
+                        magnitude: feature.properties.mag,
+                        place: feature.properties.place,
+                        time: Date(timeIntervalSince1970: TimeInterval(feature.properties.time / 1000)),
+                        coordinates: feature.geometry.coordinates
+                    )
+                }
+            }
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print("Failed to fetch earthquakes: \(error)")
+                }
+            }, receiveValue: { [weak self] earthquakes in
+                self?.earthquakes = earthquakes
+            })
+            .store(in: &cancellables)
+    }
+}
