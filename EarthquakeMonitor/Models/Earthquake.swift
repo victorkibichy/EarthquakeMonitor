fd637e4bd3bcacdb148aaf73598080ab18dff9d1
victor <victorkibichy>
2024-06-14 12:14:46 +0300

    The earthquake locations now visible in the mao

--- EarthquakeMonitor/Models/Earthquake.swift
+++ EarthquakeMonitor/Models/Earthquake.swift
@@ -6,17 +6,44 @@
 //
 
 import Foundation
-
+import CoreLocation
 import Foundation
-
-// DECLARING THE DATA TO CONFORM TO THE CODABLE PROTOCOL
-
 
 struct Earthquake: Codable {
     let magnitude: Double
     let place: String
     let time: Date
-    let coordinates: [Double] // this is the coordinates tthat pend modifications [longitude, latitude, depth]
+    let coordinates: [Double] // Keep coordinates as [Double]
+
+    init(magnitude: Double, place: String, time: Date, coordinates: [Double]) {
+        self.magnitude = magnitude
+        self.place = place
+        self.time = time
+        self.coordinates = coordinates
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case magnitude = "mag"
+        case place
+        case time
+        case coordinates = "geometry"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        magnitude = try container.decode(Double.self, forKey: .magnitude)
+        place = try container.decode(String.self, forKey: .place)
+        time = try container.decode(Date.self, forKey: .time)
+        coordinates = try container.decode([Double].self, forKey: .coordinates)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(magnitude, forKey: .magnitude)
+        try container.encode(place, forKey: .place)
+        try container.encode(time, forKey: .time)
+        try container.encode(coordinates, forKey: .coordinates)
+    }
 }
 
 struct EarthquakeResponse: Codable {
--- EarthquakeMonitor/ViewControllers/MapViewController.swift
+++ EarthquakeMonitor/ViewControllers/MapViewController.swift
@@ -1,17 +1,19 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  EarthquakeMonitor
 //
 //  Created by  Bouncy Baby on 6/12/24.
 //
 
+import UIKit
+import MapKit
 
 import UIKit
-
 import MapKit
 
 class MapViewController: UIViewController {
     var mapView: MKMapView!
+    private var earthquakes: [Earthquake] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +21,7 @@ class MapViewController: UIViewController {
         title = "Earthquakes History"
         
         setupMapView()
+        fetchEarthquakeData() // this is toFetch and display earthquake data on map
     }
     
     private func setupMapView() {
@@ -33,16 +36,65 @@ class MapViewController: UIViewController {
             mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
         
-        
         mapView.showsUserLocation = true
         mapView.showsTraffic = true
         mapView.showsScale = true
         mapView.showsCompass = true
     }
     
-    override func loadView() {
-        super.loadView()
+    private func fetchEarthquakeData() {
+        guard let url = URL(string: "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2022-01-01&endtime=2022-01-02") else {
+            print("Invalid URL")
+            return
+        }
         
+        let task = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
+            guard let data = data, error == nil else {
+                print("Failed to fetch earthquake data:", error?.localizedDescription ?? "Unknown error")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let earthquakeResponse = try decoder.decode(EarthquakeResponse.self, from: data)
+                
+                DispatchQueue.main.async {
+                    self?.earthquakes = earthquakeResponse.features.map { feature in
+                        Earthquake(
+                            magnitude: feature.properties.mag,
+                            place: feature.properties.place,
+                            time: Date(timeIntervalSince1970: TimeInterval(feature.properties.time / 1000)),
+                            coordinates: feature.geometry.coordinates
+                        )
+                    }
+                    
+                    self?.addAnnotations()
+                }
+            } catch {
+                print("Failed to decode earthquake data:", error)
+            }
+        }
         
+        task.resume()
+    }
+    
+    private func addAnnotations() {
+        mapView.removeAnnotations(mapView.annotations)
+        
+        for earthquake in earthquakes {
+            let annotation = MKPointAnnotation()
+            
+            guard earthquake.coordinates.count >= 2 else {
+                continue
+            }
+            
+            annotation.coordinate = CLLocationCoordinate2D(latitude: earthquake.coordinates[1], longitude: earthquake.coordinates[0])
+            annotation.title = "Magnitude: \(earthquake.magnitude)"
+            annotation.subtitle = earthquake.place
+            mapView.addAnnotation(annotation)
+        }
+        
+        // Optionally, you can zoom the map to fit all annotations
+        mapView.showAnnotations(mapView.annotations, animated: true)
     }
 }
