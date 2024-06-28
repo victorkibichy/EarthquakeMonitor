834219f942343249262dd4a4188303d686c84031
victor <victorkibichy>
2024-06-28 09:07:23 +0300

    Map functions in the Network Manager

--- EarthquakeMonitor/Managers/NetworkManager.swift
+++ EarthquakeMonitor/Managers/NetworkManager.swift
@@ -27,5 +27,21 @@ class NetworkManager {
             .decode(type: EarthquakeResponse.self, decoder: JSONDecoder())
             .eraseToAnyPublisher()
     }
-    
+    // Fetch Earthquake data from USGS API
+        func fetchEarthquakeData() -> AnyPublisher<EarthquakeResponse, Error> {
+            let urlString = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
+            guard let url = URL(string: urlString) else {
+                return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+            }
+            
+            return URLSession.shared.dataTaskPublisher(for: url)
+                .tryMap { result -> Data in
+                    guard let httpResponse = result.response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                        throw URLError(.badServerResponse)
+                    }
+                    return result.data
+                }
+                .decode(type: EarthquakeResponse.self, decoder: JSONDecoder())
+                .eraseToAnyPublisher()
+        }
 }
